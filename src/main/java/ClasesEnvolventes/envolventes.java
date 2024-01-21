0c0b8ac6301f464fe1bdc9418fbc85951b9dc772
ednaleiden <ednadupont@hotmail.com>
2024-01-20 22:13:05 -0500

    envolventes clase

---
+++ src/main/java/ClasesEnvolventes/envolventes.java
@@ -0,0 +1,19 @@
+package ClasesEnvolventes;
+
+import org.example.Interface.Interfaces;
+
+public class envolventes {
+
+    //sirven para almaenar un dato primitivo en objeto como es
+    //int con Interfaces
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        Integer n1 = 11; // alacena como objeto
+
+        System.out.println(n + n1);
+
+        String ns = n1.toString();// se puede convertir con estos metodos ejemplo astring
+    }
+}
