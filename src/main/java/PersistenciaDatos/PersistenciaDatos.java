0dc3eb30fd8ba01ea61e90295a722b6e7be99ae8
ednaleiden <ednadupont@hotmail.com>
2024-01-25 16:49:05 -0500

    envolventes clase

--- src/main/java/PersistenciaDatos/PersistenciaDatos.java
+++ src/main/java/PersistenciaDatos/PersistenciaDatos.java
@@ -1,8 +1,22 @@
 package PersistenciaDatos;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class PersistenciaDatos {
-    List lista1 = new ArrayList();
+    public static void main(String[] args) {
+        List lista1 = new ArrayList();
+        lista1.add("Edna");
+        lista1.add(1);
+
+      for(Object datos :lista1){
+          System.out.println(lista1);
+      }
+
+        //SET O HashSet PARA CREAR DATOS
+        //MAP
+
+        //jsp java server psp
+}
 }
